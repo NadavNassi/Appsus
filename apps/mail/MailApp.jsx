b554e845952d279e5ab8f7d1ea5f49bf898815d4
Nadav Nassi <nadav.nassi@gmail.com>
2021-05-04 18:59:11 +0300

    mail filter

--- apps/mail/MailApp.jsx
+++ apps/mail/MailApp.jsx
@@ -2,13 +2,14 @@ import { MailList } from "./cmps/MailList.jsx";
 import { Loader } from "../../cmps/Loader.jsx";
 import { mailService } from "./services/mail.service.js";
 import { MailFilter } from "./cmps/MailFilter.jsx";
+import { eventBusService } from "../../services/event.bus.service.js";
 
 export class MailApp extends React.Component {
   state = {
     mails: null,
     filterBy: {
       txt: "",
-      mailStatus: "all",
+      mailStatus: "",
     },
   };
 
@@ -17,23 +18,35 @@ export class MailApp extends React.Component {
   }
 
   loadMails = () => {
-    mailService
-      .query(this.state.filterBy)
-      .then((mails) => this.setState({ mails }));
+    mailService.query(this.state.filterBy).then((mails) => {
+      this.setState({ mails });
+      const unreadMail = mails.filter((mail) => mail.isRead === false);
+      eventBusService.emit("mail-count", {
+        mailCount: mails.length,
+        unreadMailCount: unreadMail.length,
+      });
+    });
   };
 
   onShowMail = (mailId) => {
     const { mails } = this.state;
     const chosenMailIdx = mails.findIndex((mail) => mail.id === mailId);
     mails[chosenMailIdx].isRead = true;
-    this.setState((prevState) => ({ mails }));
+    this.setState({ mails });
   };
 
   onSetFilter = (filterBy) => {
-    this.setState(
-      { filterBy: { ...this.state.filterBy, ...filterBy } },
-      this.loadMail
-    );
+    console.log(filterBy);
+    this.setState({ filterBy: { ...this.state.filterBy, ...filterBy } }, () => {
+      this.loadMails();
+    });
+  };
+
+  onDeleteMail = () => {
+    const { mailId } = this.props.match.params;
+    mailService.deleteMail(mailId).then(() => {
+      this.props.history.push("/mail");
+    });
   };
 
   render() {
--- apps/mail/cmps/MailFilter.jsx
+++ apps/mail/cmps/MailFilter.jsx
@@ -1,35 +1,66 @@
+import { eventBusService } from "../../../services/event.bus.service.js";
+
 export class MailFilter extends React.Component {
   state = {
     filterBy: {
       txt: "",
-      mailStatus: "all",
+      mailStatus: "",
     },
+    mailCount: 0,
+    unreadMailCount: 0,
   };
+  removeEvent;
+  componentDidMount() {
+    this.removeEvent = eventBusService.on(
+      "mail-count",
+      ({ mailCount, unreadMailCount }) => {
+        this.setState({ mailCount, unreadMailCount });
+      }
+    );
+  }
 
   handleChange = ({ target }) => {
     const field = target.name;
     const value = target.value;
-    this.setState(
-      ({ filterBy }) => ({
-        filterBy: { ...filterBy, [field]: value },
-      }),
-      () => {
-        this.props.onSetFilter(this.state.filterBy);
-      }
-    );
+    this.setState(({ filterBy }) => ({
+      filterBy: { ...filterBy, [field]: value },
+    }));
+  };
+
+  onFilter = (ev) => {
+    ev.preventDefault();
+    this.props.onSetFilter(this.state.filterBy);
   };
 
   render() {
     const { txt, mailStatus } = this.state.filterBy;
     return (
-      <form className='mail-filter'>
-        <h5>Search for mails: </h5>
+      <form
+        className='mail-filter flex space-evenly center'
+        onSubmit={this.onFilter}
+      >
+        <h4>Search for mails: </h4>
         <input
           type='text'
           value={txt}
           name='txt'
           onChange={this.handleChange}
+          placeholder='type to search'
         />
+        <select
+          name='mailStatus'
+          value={mailStatus}
+          onChange={this.handleChange}
+        >
+          <option value=''>All</option>
+          <option value={true}>Read</option>
+          <option value={false}>Unread</option>
+        </select>
+        <button>Search</button>
+        <p>
+          You have {this.state.unreadMailCount} of {this.state.mailCount} total
+          mails
+        </p>
       </form>
     );
   }
