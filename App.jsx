9aae78716af3a2344ce2ac1318e29642eb87888f
Nadav Nassi <nadav.nassi@gmail.com>
2021-05-04 20:07:33 +0300

    conflict resolve

--- App.jsx
+++ App.jsx
@@ -1,14 +1,6 @@
 const Router = ReactRouterDOM.HashRouter;
 const { Route, Switch } = ReactRouterDOM;
 
-<<<<<<< HEAD
-import { MailDetails } from "./apps/mail/pages/MailDetails.jsx";
-import { AppHeader } from "./cmps/AppHeader.jsx";
-import { BookApp } from "./apps/book/BookApp.jsx";
-import { MailApp } from "./apps/mail/MailApp.jsx";
-import { NoteApp } from "./apps/note/NoteApp.jsx";
-import { Home } from "./pages/Home.jsx";
-=======
 import { MailDetails } from './apps/mail/pages/MailDetails.jsx';
 import { AppHeader } from './cmps/AppHeader.jsx';
 import { BookApp } from './apps/book/BookApp.jsx';
@@ -16,7 +8,6 @@ import { MailApp } from './apps/mail/MailApp.jsx';
 import { NoteApp } from './apps/note/NoteApp.jsx';
 import { NoteModal } from './apps/note/cmps/NoteModal.jsx';
 import { Home } from './pages/Home.jsx';
->>>>>>> a3e15067b6b8f780d0027c1adb0714d73a0e48aa
 
 
 export function App() {
