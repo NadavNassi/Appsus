2502f13b30611bf63bf35a7254256ec2513b641e
matan1542 <matan8380@gmail.com>
2021-05-05 09:49:47 +0300

    Color Line

--- apps/note/cmps/EditLine.jsx
+++ apps/note/cmps/EditLine.jsx
@@ -1,6 +1,6 @@
 
 const { Route,Link,withRouter } = ReactRouterDOM;
-
+import {ColorLine} from './ColorLine.jsx';
 import {NoteModal} from './NoteModal.jsx'; 
  class _EditLine extends React.Component {
     state = {
@@ -17,7 +17,9 @@ import {NoteModal} from './NoteModal.jsx';
                         onPinnedNote(id)
                     }    
                     }><i className="fas fa-thumbtack"></i></button>
-                    <button className="note-btn"><i className="fas fa-palette"></i></button>
+                    <button className="note-btn" onClick={()=>{
+                        <ColorLine/>
+                    }}><i className="fas fa-palette"></i></button>
                     <Route component={()=><NoteModal onEditNote={onEditNote}/>} path="/note/edit/:id" />
                     <Link className="note-btn" to={`/note/edit/${id}`} ><i className="fas fa-edit"></i></Link>
                     <button onClick={() =>{
--- apps/note/cmps/NotePreview.jsx
+++ apps/note/cmps/NotePreview.jsx
@@ -1,15 +1,29 @@
-import {EditLine} from './EditLine.jsx'
+import { EditLine } from "./EditLine.jsx";
 export class NotePreview extends React.Component {
-    state = {}
-    render(){
-        const {note,onDeleteNote,onEditNote,onPinnedNote} = this.props;
-        return(
-            <section>
-                <div className="note-container">
-                    <p>{note.info.txt}</p>
-                    <EditLine id={note.id} onDeleteNote={onDeleteNote} onPinnedNote={onPinnedNote} onEditNote={onEditNote}/>
-                </div>
-            </section>
-        )
-    }
+  state = {
+    colorProfile: "null",
+  };
+  componentDidMount() {
+   
+  }
+  changeColorProfile = (color) => {
+
+  };
+  render() {
+    const { note, onDeleteNote, onEditNote, onPinnedNote } = this.props;
+    return (
+      <section>
+        <div className={`note-container ${this.state.colorProfile}`} ref={this.noteContainer}>
+          <p>{note.info.txt}</p>
+          <EditLine
+            id={note.id}
+            onDeleteNote={onDeleteNote}
+            changeColorProfile={this.changeColorProfile}
+            onPinnedNote={onPinnedNote}
+            onEditNote={onEditNote}
+          />
+        </div>
+      </section>
+    );
+  }
 }
