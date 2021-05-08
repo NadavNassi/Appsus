16e0cd1f386ab1fd8a506b78dc6607f449a8f118
matan1542 <matan8380@gmail.com>
2021-05-08 16:22:41 +0300

    home-page

--- apps/note/services/storage/notes.js
+++ apps/note/services/storage/notes.js
@@ -5,7 +5,7 @@ export const notes = [
         id:utilService.makeId(),
         type: "NoteVideo",
         isPinned: false,
-        color:null,
+        color:'light-green',
         info: {
             video: "https://www.youtube.com/watch?v=z9eoubnO-pE&t=199s"
         }
@@ -13,7 +13,7 @@ export const notes = [
         id:utilService.makeId(),
         type: "NoteText",
         isPinned: false,
-        color:null,
+        color:'indigo',
         info: {
             txt: "Nadav likes css very much"
         }
@@ -29,7 +29,7 @@ export const notes = [
         id:utilService.makeId(),
         type: "NoteMap",
         isPinned: false,
-        color:null,
+        color:'blue',
         info: {
             map: "Tel Aviv!"
         }
@@ -53,7 +53,7 @@ export const notes = [
         id:utilService.makeId(),
         type: "NoteImg",
         isPinned: false,
-        color:null,
+        color:'light-purple',
         info: {
             img: "https://images.pexels.com/photos/207962/pexels-photo-207962.jpeg?cs=srgb&dl=pexels-pixabay-207962.jpg&fm=jpg"
         }
@@ -62,7 +62,7 @@ export const notes = [
         id:utilService.makeId(),
         type: "NoteTodos",
         isPinned: false,
-        color:null,
+        color:'light-orange',
         info: {
             list: "Matan, Nadav,Harel,Ilai"
         }
